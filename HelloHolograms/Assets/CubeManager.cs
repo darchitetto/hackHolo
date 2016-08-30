bb78a745fa2d2f9debfd8adaeec0197e48fa2b2a
JimW <jwallace@corp.versionone.net>
2016-08-30 13:56:06 -0400

    re-add helloholo

---
+++ HelloHolograms/Assets/CubeManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR.WSA.Input;
+
+public class CubeManager : MonoBehaviour
+{
+	public GameObject blueCubePrefab;
+
+	GestureRecognizer recognizer;
+
+	void Start()
+	{
+		recognizer = new GestureRecognizer();
+
+		recognizer.TappedEvent += Recognizer_TappedEvent;
+
+		recognizer.StartCapturingGestures();
+	}
+
+	private void Recognizer_TappedEvent(InteractionSourceKind source, int tapCount, Ray headRay)
+	{
+		var direction = headRay.direction;
+
+		var origin = headRay.origin;
+
+		var position = origin + direction * 2.0f;
+
+		Instantiate(blueCubePrefab, position, Quaternion.identity);
+	}
+}
---
+++ HelloHolograms/Assets/Scripts/PlaceOnTap.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlaceOnTap : MonoBehaviour
+{
+	public GameObject cubeToPlace;
+
+	// Called by GazeGestureManager when the user performs a Select gesture
+	void OnSelect()
+	{
+		var direction = Camera.main.transform.forward;
+
+		var origin = Camera.main.transform.position;
+
+		var position = origin + direction * 2.0f;
+
+		Instantiate(cubeToPlace, position, Quaternion.identity);
+	}
+
+}
