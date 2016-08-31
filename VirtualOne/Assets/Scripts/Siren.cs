68a56c143679932b6ea2a79a42f2e67b250aea7c
Kevin Marsh <kevin.marsh@versionone.com>
2016-08-31 11:43:26 -0400

    Siren in working condition

---
+++ VirtualOne/Assets/Scripts/Siren.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Siren : MonoBehaviour
+{
+	private Renderer rend;
+	private bool isOn;
+	private int count;
+	private Color darkred = new Color32(150,0,0,1);
+	private Color lightred = new Color32(255,50,50,1);
+	// Use this for initialization
+	void Start () {
+		rend = this.GetComponent<Renderer>();
+		isOn = false;
+		rend.material.color = Color.green;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (isOn && count>35)
+		{
+			var color = rend.material.color;
+			if (color == lightred)
+			{
+				rend.material.color = darkred;
+			}
+			else
+			{
+				rend.material.color = lightred;
+			}
+
+			count = 0;
+		}
+		count++;
+	}
+
+	void OnSelect()
+	{
+		ToggleOnOff();
+	}
+
+	public void ToggleOnOff()
+	{
+		if (isOn)
+		{
+			rend.material.color = Color.green;
+		}
+		else
+		{
+			rend.material.color = Color.red;
+		}
+		isOn = !isOn;
+	}
+}
---
+++ VirtualOne/Assets/Scripts/SirenSound.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SirenSound : MonoBehaviour {
+	private AudioSource audioSource = null;
+	private bool isOn;
+	// Use this for initialization
+	void Start () {
+		audioSource = gameObject.AddComponent<AudioSource>();
+		audioSource.playOnAwake = false;
+		audioSource.spatialize = true;
+		audioSource.spatialBlend = 1.0f;
+		audioSource.dopplerLevel = 1.0f;
+		audioSource.clip = Resources.Load<AudioClip>("Siren_Noise");
+		audioSource.loop = true;
+		isOn = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ToggleOnOff()
+	{
+		if (isOn)
+		{
+			audioSource.Stop();
+		}
+		else
+		{
+			audioSource.Play();
+		}
+		isOn = !isOn;
+	}
+}
