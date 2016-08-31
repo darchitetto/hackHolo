793e101a2cc48bab7c718807793c57bf44af8e4b
darchitetto <denise@digital-pioneers.com>
2016-08-31 16:13:51 -0400

    Dynamic Pyramid

---
+++ VirtualOne/Assets/Scripts/CreatePyramid.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreatePyramid : MonoBehaviour {
+
+
+	[Tooltip("Game object to place when tapping")]
+	public GameObject startGame;
+
+	public GameObject testToPlace;
+	public GameObject taskToPlace;
+	public GameObject defectToPlace;
+	public GameObject storyToPlace;
+	public GameObject epicToPlace;
+	public GameObject projectToPlace;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void OnSelect () {
+		
+		Instantiate(taskToPlace, new Vector3((float).0, 0, 0), Quaternion.identity);
+		Instantiate(taskToPlace, new Vector3((float).25, 0, 0), Quaternion.identity);
+		Instantiate(taskToPlace, new Vector3((float).5, 0, 0), Quaternion.identity);
+		Instantiate(taskToPlace, new Vector3((float).75, 0, 0), Quaternion.identity);
+		Instantiate(taskToPlace, new Vector3((float)1, 0, 0), Quaternion.identity);
+		Instantiate(taskToPlace, new Vector3((float)1.25, 0, 0), Quaternion.identity);
+
+		Instantiate(testToPlace, new Vector3((float).125, (float) .25, 0), Quaternion.identity);
+		Instantiate(testToPlace, new Vector3((float).375, (float).25, 0), Quaternion.identity);
+		Instantiate(testToPlace, new Vector3((float).625, (float).25, 0), Quaternion.identity);
+		Instantiate(testToPlace, new Vector3(1, (float).875, 0), Quaternion.identity);
+		Instantiate(testToPlace, new Vector3((float)1.125, (float).25, 0), Quaternion.identity);
+
+		Instantiate(defectToPlace, new Vector3((float).25, (float).5, 0), Quaternion.identity);
+		Instantiate(defectToPlace, new Vector3((float).5, (float).5, 0), Quaternion.identity);
+		Instantiate(defectToPlace, new Vector3((float).75, (float).5, 0), Quaternion.identity);
+		Instantiate(defectToPlace, new Vector3(1, (float).5, 0), Quaternion.identity);
+
+		Instantiate(storyToPlace, new Vector3((float).375, 1, 0), Quaternion.identity);
+		Instantiate(storyToPlace, new Vector3((float).625, 1, 0), Quaternion.identity);
+		Instantiate(storyToPlace, new Vector3((float).875, 1, 0), Quaternion.identity);
+
+		Instantiate(epicToPlace, new Vector3((float).625, (float)1.25, 0), Quaternion.identity);
+		Instantiate(epicToPlace, new Vector3((float).875, (float)1.25, 0), Quaternion.identity);
+
+		Instantiate(projectToPlace, new Vector3((float)1.25, (float)1.5, 0), Quaternion.identity);
+
+	}
+
+
+}
