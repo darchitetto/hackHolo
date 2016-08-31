ec43d01d74a92a2178f8b6ae784360185c3f2b30
JimW <jwallace@corp.versionone.net>
2016-08-31 11:34:59 -0400

    Update place on tap to move when placing

--- VirtualOne/Assets/Scripts/PlaceOnTap.cs
+++ VirtualOne/Assets/Scripts/PlaceOnTap.cs
@@ -5,19 +5,53 @@ public class PlaceOnTap : MonoBehaviour
     [Tooltip("Game object to place when tapping")]
 	public GameObject cubeToPlace;
 
-    [Tooltip("Distance in front of camera.")]
-    public float CameraDistance = 2.0f;
+    [Tooltip("Distance from object.")]
+    public float Distance = 2.0f;
+
+    [Tooltip("Immediately move the object")]
+    public bool MoveOnPlace = true;
+
+    void OnSelect()
+    {
+        if (MoveOnPlace)
+            OnSelectNextToCamera();
+        else
+            OnSelectNextToObject();
+    }
 
     // Called by GazeGestureManager when the user performs a Select gesture
-    void OnSelect()
+    void OnSelectNextToObject()
+    {
+        // this makes the objet face the camera
+        // transform.LookAt(Camera.main.transform);
+        // Camera.main.transform.LookAt(transform);
+
+
+        // position new object relative to this object
+        var direction = transform.forward;
+
+        var origin = transform.position;
+
+
+        var position = origin + direction * Distance;
+
+        var obj = Instantiate(cubeToPlace, position, Quaternion.identity);
+    }
+
+    // this positions the new object relative to camera
+    void OnSelectNextToCamera()
 	{
 		var direction = Camera.main.transform.forward;
 
 		var origin = Camera.main.transform.position;
 
-		var position = origin + direction * CameraDistance;
+		var position = origin + direction * Distance;
 
-		Instantiate(cubeToPlace, position, Quaternion.identity);
+        var obj = Instantiate(cubeToPlace, position, Quaternion.identity);
+        if ( obj is GameObject)
+        {
+            (obj as GameObject).SendMessage("OnSelect");
+        }
 	}
 
 }
