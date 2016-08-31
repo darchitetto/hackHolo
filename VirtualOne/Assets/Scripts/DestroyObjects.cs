5fa7a3ddb1526b28554b14a18c8205b41fd78072
JimW <jwallace@corp.versionone.net>
2016-08-31 15:37:33 -0400

    add DestroyObjects

---
+++ VirtualOne/Assets/Scripts/DestroyObjects.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public class DestroyObjects : MonoBehaviour
+{
+    [Tooltip("Object name regex to find and destroy")]
+	public string ObjectNameRegex;
+
+    void OnSelect()
+    {
+        Regex r = new Regex(ObjectNameRegex);
+
+        foreach (var o in FindObjectsOfType<GameObject>().Where(o => r.IsMatch( o.name )))
+            Destroy(o);
+    }
+
+}
