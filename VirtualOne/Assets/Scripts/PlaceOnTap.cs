6cc7882d15e06e6d0f226d3b1d4173accb3758b4
JimW <jwallace@corp.versionone.net>
2016-08-31 09:57:26 -0400

    add placement scripts

---
+++ VirtualOne/Assets/Scripts/PlaceOnTap.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlaceOnTap : MonoBehaviour
+{
+    [Tooltip("Game object to place when tapping")]
+	public GameObject cubeToPlace;
+
+    [Tooltip("Distance in front of camera.")]
+    public float CameraDistance = 2.0f;
+
+    // Called by GazeGestureManager when the user performs a Select gesture
+    void OnSelect()
+	{
+		var direction = Camera.main.transform.forward;
+
+		var origin = Camera.main.transform.position;
+
+		var position = origin + direction * CameraDistance;
+
+		Instantiate(cubeToPlace, position, Quaternion.identity);
+	}
+
+}
